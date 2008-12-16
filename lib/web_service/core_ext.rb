334116a29987ea3b87fb460cea78bc4898722ff8
Roman Le Negrate <roman.lenegrate@gmail.com>
2008-12-16 15:13:53 +0100

    Moved core extensions (including Array#index with support for block form) to their own file.  Escape ID's when constructing URL's since they're not necessarily integers.

---
+++ lib/web_service/core_ext.rb
@@ -0,0 +1,36 @@
+Array.class_eval do
+  alias_method :index_without_block_form, :index
+  def index(*args, &block)
+    if args.empty? && block
+      each_with_index { |obj, idx| return idx if block.call(obj) }; nil
+    elsif args.size == 1
+      index_without_block_form(*args, &block)
+    else
+      raise ArgumentError, "wrong number of arguments (%d for 1)" % args.size
+    end
+  end
+end if Array.instance_method(:index).arity == 1
+
+Object.class_eval do
+  undef :id if method_defined?(:id)
+end
+
+def URI(object)
+  URI === object ? object : URI.parse(object.to_s)
+end
+
+URI.class_eval do
+  def obfuscate
+    returning(dup) do |obfuscated|
+      obfuscated.user &&= '***'
+      obfuscated.password &&= '***'
+    end
+  end
+end
+
+class << CGI
+  alias_method :old_escape, :escape
+  def escape(string)
+    old_escape(string).gsub(/\./, '%' + '.'.unpack('H2')[0])
+  end
+end
---
+++ test/web_service/core_ext_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class CoreExtTest < Test::Unit::TestCase
+  def test_uri_obfuscate
+    assert_equal "http://***:***@example.com", URI("http://foo:secret@example.com").obfuscate.to_s
+  end
+  
+  def test_cgi_escape
+    assert_equal "example%2ecom", CGI.escape("example.com")
+  end
+end
