bb00edf1e2c158fa55b4a3b03af8d7937ef7e060
Roman Le Negrate <roman.lenegrate@gmail.com>
2008-12-21 18:52:42 +0100

    remove parenthesis for arguments to 'raise', in order to remove some noise

--- lib/web_service/response_handling.rb
+++ lib/web_service/response_handling.rb
@@ -102,31 +102,31 @@ module WebService
     def handle_response(response)
       case response.code.to_i
         when 301,302
-          raise(Redirection.new(response))
+          raise Redirection.new(response)
         when 200...400
           response
         when 400
-          raise(BadRequest.new(response))
+          raise BadRequest.new(response)
         when 401
-          raise(UnauthorizedAccess.new(response))
+          raise UnauthorizedAccess.new(response)
         when 403
-          raise(ForbiddenAccess.new(response))
+          raise ForbiddenAccess.new(response)
         when 404
-          raise(ResourceNotFound.new(response))
+          raise ResourceNotFound.new(response)
         when 405
-          raise(MethodNotAllowed.new(response))
+          raise MethodNotAllowed.new(response)
         when 406
-          raise(NotAcceptable.new(response))
+          raise NotAcceptable.new(response)
         when 409
-          raise(ResourceConflict.new(response))
+          raise ResourceConflict.new(response)
         when 422
-          raise(ResourceInvalid.new(response))
+          raise ResourceInvalid.new(response)
         when 401...500
-          raise(ClientError.new(response))
+          raise ClientError.new(response)
         when 500...600
-          raise(ServerError.new(response))
+          raise ServerError.new(response)
         else
-          raise(ConnectionError.new(response, "Unknown response code: #{response.code}"))
+          raise ConnectionError.new(response, "Unknown response code: #{response.code}")
       end
     end
   end
